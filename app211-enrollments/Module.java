77e551ad73347c85875f901b8d8c3ff40ed25359
DanielChorlton <91318755+DanielChorlton@users.noreply.github.com>
2021-10-05 12:28:57 +0100

    setup variables and constructor from method class

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (Daniel Chorlton)
+ * @version (v1.0 05-10-2021)
+ */
+public class Module
+{
+    // instance variables - created variables for class
+    private String code;
+    private String title;
+    private int value;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+        this.code = code;
+        this.title = title;
+        this.value = value;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
